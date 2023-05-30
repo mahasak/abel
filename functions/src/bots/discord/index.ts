30f4833bc2065e99472479afbedd02d23a5d476e
Mahasak Pijittum <mahasak@gmail.com>
2023-05-30 17:11:20 +0800

    :bug: Fix bug in initialize

--- functions/src/bots/discord/index.ts
+++ functions/src/bots/discord/index.ts
@@ -1,17 +1,22 @@
 import * as express from "express";
 import * as functions from "firebase-functions";
 import { Client, GatewayIntentBits } from "discord.js";
-let client: any = null;
+let client: any = undefined;
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN ?? "";
 const DISCORD_CLIENT_PUBLIC_KEY = process.env.DISCORD_CLIENT_PUBLIC_KEY ?? "";
 import { InteractionResponseType, InteractionType, verifyKey } from "discord-interactions";
 
 export const discordInteractionHandler = async (req: functions.https.Request, res: express.Response) => {
-  // Initialize discord client for messageCreate
+  // warmup URL for only initialize feature
   if (!client) {
-    console.log("initialize client");
     initializeDiscordClient(req);
   }
+  console.log(req.query["warmup"])
+  if (req.query["warmup"] && req.query["warmup"] == "1") {
+    res.status(200).end("Done Client warmup");
+    return;
+  }
+
 
   const signature = req.get("X-Signature-Ed25519") ?? "";
   const timestamp = req.get("X-Signature-Timestamp") ?? "";
@@ -49,6 +54,9 @@ export const discordInteractionHandler = async (req: functions.https.Request, re
 }
 
 const initializeDiscordClient = (req: functions.https.Request) => {
+
+  console.log("initialize client");
+  console.log("Test")
   client = new Client({ intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds] });
 
   client.once("ready", () => {
@@ -64,12 +72,13 @@ const initializeDiscordClient = (req: functions.https.Request) => {
 
     // Our first command
     switch (command) {
-    case "!hello":
-      message.channel.send("world!");
-      break;
+      case "!hello":
+        message.channel.send("world!");
+        break;
     }
     // you can add more commands in switch cases.
   });
 
   client.login(DISCORD_BOT_TOKEN);
+
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,23 @@
-firebase functions:config:set abel="$(cat ./functions/.config.json)"
+# ABEL
+
+## How to install
+
+### environment variable
+Setup by `.env` file
+
+### cloud function 403 forbidden error
+If you are getting 403 forbidden error like below
+
+Error: Forbidden Your client does not have permission to get URL /api/test from this server.
+
+Please follow below steps to grant access to all users. Basically this is to allow unauthenticated clients to access your api endpoint.
+
+Go to https://console.cloud.google.com/functions/list
+Select the function to which you want to give public access
+Click on PERMISSIONS
+Click on ADD MEMBER
+Type allUsers
+Select role Cloud Functions -> Cloud Functions Invoker
+Save
+That's it, now test your api.
+see documentation: https://cloud.google.com/functions/docs/securing/managing-access-iam#allowing_unauthenticated_function_invocation
